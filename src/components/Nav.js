f43c9a691d271da83e4ee59a4518da248d3c3791
David Robison <david@robisonweb.dev>
2021-06-29 14:14:22 -0600

    Moved h3 inbetween Link Component. Set Link's to /. Gave Link className of siteNavTitle.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
     return (
         <nav id='nav'>
-            <h3>David Robison</h3>
+            <Link className='siteNavTitle' to='/'><h3>David Robison</h3></Link>
             <ul id='navLinks'>
                 <li>Projects</li>
                 <li>About</li>
