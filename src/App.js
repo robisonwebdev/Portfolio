a66fb3f1fe255949558fea04552f786362056a25
David Robison <david@robisonweb.dev>
2021-06-30 17:25:40 -0600

    Imported Projects, About, and Contact components. Put component between their Route components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
+import Projects from './components/Projects';
+import About from './components/About';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
@@ -8,9 +11,15 @@ const App = () => {
       <div id='App'>
         <Nav />
         <Switch>
-          <Route exact path='/'></Route>
-          <Route exact path='/about'></Route>
-          <Route exact path='/contact'></Route>
+          <Route exact path='/'>
+            <Projects />
+          </Route>
+          <Route exact path='/about'>
+            <About />
+          </Route>
+          <Route exact path='/contact'>
+            <Contact />
+          </Route>
         </Switch>
       </div>
     </Router>
