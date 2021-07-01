4b30ac65fc1e03d825b4334498b1eb18f0ad3780
David Robison <david@robisonweb.dev>
2021-07-01 13:09:01 -0600

    Changed href to accept props.address

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = (props) => {
     return (
         <footer>
-            <small>{props.text}<a href='https://github.com/robisonwebdev' target='_blank' rel='noreferrer'>{props.name}</a></small>
+            <small>{props.text}<a href={props.address} target='_blank' rel='noreferrer'>{props.name}</a></small>
         </footer>
     );
 }
