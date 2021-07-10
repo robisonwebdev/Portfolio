7bed712fe447095f32a58b8791a9efeacf96f57f
David Robison <david@robisonweb.dev>
2021-07-10 13:53:49 -0600

    Added FooterCard component with iconAddress for linkedin profile and iconClass for linkedin icon.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = (props) => {
     return (
         <footer>
             <FooterCard iconAddess='https://github.com/robisonwebdev' iconClass='lab la-github' />
+            <FooterCard iconAddess='https://www.linkedin.com/in/davidrulonrobison/' iconClass='lab la-linkedin' />
         </footer>
     );
 }
