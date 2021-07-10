67ebfbd03e6bef66be6deabaa3762a9d2041d171
David Robison <david@robisonweb.dev>
2021-07-10 13:58:08 -0600

    Added div with className developedBy, with 'p' element

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import FooterCard from './FooterCard';
 
-const Footer = (props) => {
+const Footer = () => {
     return (
         <footer>
             <div className='footerCards'>
                 <FooterCard iconAddess='https://github.com/robisonwebdev' iconClass='lab la-github' />
                 <FooterCard iconAddess='https://www.linkedin.com/in/davidrulonrobison/' iconClass='lab la-linkedin' />
+            </div>
+            <div className='developedBy'>
+                <p>David Robison &#169;2021</p>
             </div>
         </footer>
     );
