005b7288781ea80826eb0f3eebbe3ee68a51b7fa
David Robison <david@robisonweb.dev>
2021-07-12 12:48:23 -0600

    Added a tag, with img tag inside. Destructered props for img and a tag's. Added props to img and a tag's.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const ProjectCard = ({ title, details }) => {
+const ProjectCard = ({ image, imageAlt, address, title, details }) => {
     return (
         <div className='projectCard'>
-            <div className='projectCardImage'></div>
+            <div className='projectCardImage'>
+                <a href={address} target='_blank' rel='noreferrer'><img src={image} alt={imageAlt} /></a>
+            </div>
             <div className='projectCardInfo'>
                 <h2>{title}</h2>
                 <p>{details}</p>
