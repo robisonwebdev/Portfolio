e495d084551a304e687d688afcb30d65d588188c
David Robison <david@robisonweb.dev>
2021-07-13 13:31:50 -0600

    Imported projectInformation.js Created projectMap to map through projectInformation and return each project as <ProjectCard> Removed old ProjectCard's from projects div

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
+import { projectInformation } from '../projectInformation';
 import '../styles/Projects.css';
 
 const Projects = () => {
+    const projectMap = projectInformation.map(project => {
+        return (
+            <ProjectCard
+            image={project.image}
+            imageAlt={project.imageAlt}
+            address={project.address}
+            title={project.title}
+            details={project.details}
+        />
+        );
+    })
     return (
         <div className='projects'>
-            <ProjectCard 
-                image='https://images.unsplash.com/photo-1502945015378-0e284ca1a5be?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' 
-                imageAlt='Sample website'
-                address='https://github.com/robisonwebdev/shopping-cart'
-                title='Sample Website'
-                details='My objective for creating a Shopping Cart with react was to learn how to use Router. Router allowed me move from creating single page applications, to multiple page applications. With Router I was able to setup a navigation using HashRouter, Switch, Route, and Link.'
-            />
+            {projectMap}
+            {projectMap}
+            {projectMap}
+            {projectMap}
         </div>
     );
 }
