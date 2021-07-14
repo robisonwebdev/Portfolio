7629e46f5ca9e3dc715d245fc1ee5061212b3d85
David Robison <david@robisonweb.dev>
2021-07-14 09:32:51 -0600

    Moved Switch from App.js into Content.js Imported file need for Switch to work

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Projects from './Projects';
+import About from './About';
+import Contact from './Contact';
+import '../styles/Content.css';
 
 const Content = () => {
     return (
-        <div>Content</div>
+        <div className='content'>
+            <Switch>
+                <Route exact path='/'>
+                    <Projects />
+                </Route>
+                <Route exact path='/about'>
+                    <About />
+                </Route>
+                <Route exact path='/contact'>
+                    <Contact />
+                </Route>
+            </Switch>
+        </div>
     );
 }
 
