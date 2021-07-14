149484071c5dd70320ca6ed722074ef150ded6c0
David Robison <david@robisonweb.dev>
2021-07-14 09:35:03 -0600

    Added Footer to bottom of return div

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Footer from './Footer';
 
 const About = () => {
     return (
-        <div className='about'></div>
+        <div className='about'>
+            <Footer text='Developed by ' name='David Robison' address='https://github.com/robisonwebdev' />
+        </div>
     );
 }
 
