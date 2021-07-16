2998fc3272cb693a10d368aadb9da5ed7c8b4913
David Robison <drbr56@gmail.com>
2021-07-15 21:05:45 -0600

    Added h1 with text of My Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,6 +18,7 @@ const Projects = () => {
     })
     return (
         <div className='projects'>
+            <h1>My Projects</h1>
             {projectMap}
             {projectMap}
             {projectMap}
