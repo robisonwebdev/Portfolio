eb984059bf87d7b7cdff86dcbbef59d9385cad34
David Robison <david@robisonweb.dev>
2021-07-17 13:47:00 -0600

    Updated shopping cart information to correct data

--- src/projectInformation.js
+++ src/projectInformation.js
@@ -1,9 +1,11 @@
+import shoppingCart from './pictures/shoppingCart.png';
+
 export const projectInformation = [
     {
         'address': 'https://github.com/robisonwebdev/shopping-cart',
         'details': 'My objective for creating a Shopping Cart with react was to learn how to use Router. Router allowed me move from creating single page applications, to multiple page applications. With Router I was able to setup a navigation using HashRouter, Switch, Route, and Link.',
-        'image': 'https://images.unsplash.com/photo-1502945015378-0e284ca1a5be?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-        'imageAlt': 'Sample website',
-        'title': 'Sample website'
+        'image': shoppingCart,
+        'imageAlt': 'Shopping cart app',
+        'title': 'Shopping Cart App'
     }
-];
+]
