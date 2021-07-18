9884b02459664abf4add89fc8d07fe3354428877
David Robison <david@robisonweb.dev>
2021-07-18 15:14:46 -0600

    Fixed address for shoppingCart

--- src/projectInformation.js
+++ src/projectInformation.js
@@ -5,7 +5,7 @@ import todoApp from './pictures/todoApp.png';
 
 export const projectInformation = [
     {
-        'address': 'https://github.com/robisonwebdev/shopping-cart',
+        'address': 'https://robisonwebdev.github.io/shopping-cart/',
         'details': 'My objective for creating a Shopping Cart with react was to learn how to use Router. Router allowed me move from creating single page applications, to multiple page applications. With Router I was able to setup a navigation using HashRouter, Switch, Route, and Link.',
         'image': shoppingCart,
         'imageAlt': 'Shopping cart app',
