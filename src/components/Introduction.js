300800d6046a65db6cc4ba1eff72f1e1015985cd
David Robison <david@robisonweb.dev>
2021-07-20 15:50:11 -0600

    Added h1 and p elements inside of introLeft

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Introduction = () => {
     return (
         <div id='introduction'>
-            <div id='introLeft'></div>
+            <div id='introLeft'>
+                <h1>Welcome, I'm David</h1>
+                <p>Some text about me here</p>
+            </div>
             <div id='introRight'></div>
         </div>
     );
