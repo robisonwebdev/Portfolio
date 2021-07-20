4d8f030cbc7eb94a7963050a0ced56699362657d
David Robison <david@robisonweb.dev>
2021-07-20 15:59:54 -0600

    Removed h1 and div

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,10 +6,7 @@ import '../styles/Main.css';
 const Main = () => {
     return (
         <div className='main'>
-            <h1>Main</h1>
-            <div>
-                <Introduction />
-            </div>
+            <Introduction />
             <Footer text='Developed by ' name='David Robison' address='https://github.com/robisonwebdev' /> 
         </div>
     );
