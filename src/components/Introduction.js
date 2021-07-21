3c9322b5cc15fe833f4ae60a192b3272eac76e07
David Robison <david@robisonweb.dev>
2021-07-21 08:28:55 -0600

    Imported Introduction.css

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from './Button';
 import myPhoto from '../pictures/myPhoto.jpeg';
+import '../styles/Introduction.css';
 
 const Introduction = () => {
     return (
