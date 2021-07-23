f1e20e938638de1c315f13afa7233bec19f69619
David Robison <david@robisonweb.dev>
2021-07-23 16:41:39 -0600

    Imported Link and Linked Button component to /contact

--- src/components/IntroContact.js
+++ src/components/IntroContact.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from './Button';
+import { Link } from 'react-router-dom';
 
 const IntroContact = () => {
     return (
         <div className='introContact'>
             <div className='leftContact'>
                 <h1>Let's Work Together</h1>
-                <Button text='Get in Touch' className='introContactBtn' />
+                <Link to='/contact'><Button text='Get in Touch' className='introContactBtn' /></Link>
             </div>
             <div className='rightContact'></div>
         </div>
