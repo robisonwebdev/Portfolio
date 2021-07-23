e35a4634e92644f3eed4a38786eddae04c0f563e
David Robison <david@robisonweb.dev>
2021-07-23 16:43:29 -0600

    Imported IntroContact and added IntroContact component to main div

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Footer from './Footer';
 import Introduction from './Introduction';
 import '../styles/Main.css';
+import IntroContact from './IntroContact';
 
 const Main = () => {
     return (
         <div className='main'>
             <Introduction />
+            <IntroContact />
             <Footer text='Developed by ' name='David Robison' address='https://github.com/robisonwebdev' /> 
         </div>
     );
