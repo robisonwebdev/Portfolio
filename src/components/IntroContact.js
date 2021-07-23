567d09baf3683bf29eaacfd993dc92d51fa525f3
David Robison <david@robisonweb.dev>
2021-07-23 16:40:33 -0600

    Imported Button component. Add h1 and Button component to leftContact div. Passed props to Button.

--- src/components/IntroContact.js
+++ src/components/IntroContact.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from './Button';
 
 const IntroContact = () => {
     return (
         <div className='introContact'>
-            <div className='leftContact'></div>
+            <div className='leftContact'>
+                <h1>Let's Work Together</h1>
+                <Button text='Get in Touch' className='introContactBtn' />
+            </div>
             <div className='rightContact'></div>
         </div>
     );
