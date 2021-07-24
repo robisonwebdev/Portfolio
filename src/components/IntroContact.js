2305d914b7e6f9c497be0f9628e700321b6f7f97
David Robison <david@robisonweb.dev>
2021-07-24 17:40:35 -0600

    Imported ContactLink. Added 3 ContactLink componenets for email, github, and linkedin

--- src/components/IntroContact.js
+++ src/components/IntroContact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import ContactLink from './ContactLink';
 import { Link } from 'react-router-dom';
 
 const IntroContact = () => {
@@ -9,7 +10,11 @@ const IntroContact = () => {
                 <h1>Let's Work Together</h1>
                 <Link to='/contact'><Button text='Get in Touch' className='introContactBtn' /></Link>
             </div>
-            <div className='rightContact'></div>
+            <div className='rightContact'>
+                <ContactLink iconClass='las la-envelope' text='david@robisonweb.dev' />
+                <ContactLink iconClass='lab la-github' text='Github' />
+                <ContactLink iconClass='lab la-linkedin' text='LinkedIn' />
+            </div>
         </div>
     );
 }
