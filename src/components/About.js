58e4e0f178454fc120201073168af426fb22c85c
David Robison <david@robisonweb.dev>
2021-07-26 10:11:23 -0600

    Removed Footer from about div

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Footer from './Footer';
 import '../styles/About.css';
 
 const About = () => {
@@ -7,7 +6,6 @@ const About = () => {
         <div className='about'>
             <h1>About Me</h1>
             <div>About me component here</div>
-            <Footer text='Developed by ' name='David Robison' address='https://github.com/robisonwebdev' />
         </div>
     );
 }
