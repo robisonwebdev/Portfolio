fbe56ed812111ceb78ea9aa9bc9dd808ba9bbe1c
David Robison <david@robisonweb.dev>
2021-07-28 15:58:37 -0600

    Imported PageIntro. Added PageIntro between about div, and passed down description and title props.

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PageIntro from './PageIntro';
 import '../styles/About.css';
 
 const About = () => {
     return (
         <div className='about'>
-            <h1>About Me</h1>
+            <PageIntro description='Future text about me' title='About Me' />
             <div>About me component here</div>
         </div>
     );
