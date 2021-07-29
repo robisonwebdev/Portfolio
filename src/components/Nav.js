7660052e1b0aacb082eab17ca45c0eff64ee93a3
David Robison <david@robisonweb.dev>
2021-07-29 14:37:23 -0600

    Removed Link import. Removed all Links from nav div. Removed h3.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/Nav.css';
 
 const Nav = () => {
     return (
         <nav id='nav'>
-            <Link className='siteNavTitle' to='/'><h3>David Robison</h3></Link>
             <ul id='navLinks'>
-                <Link className='navLink' to='/projects'><li>Projects</li></Link>
-                <Link className='navLink' to='/about'><li>About</li></Link>
-                <Link className='navLink' to='/contact'><li>Contact</li></Link>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Contact</li>
             </ul>
         </nav>
     );
