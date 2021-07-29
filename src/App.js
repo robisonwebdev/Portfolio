f12f3b2352e32ce5ae4b061522d79bc0a924a0fb
David Robison <david@robisonweb.dev>
2021-07-29 14:35:10 -0600

    Imported and added Contact component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Introduction from './components/Introduction';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './styles/App.css';
 
@@ -15,6 +16,7 @@ const App = () => {
       <About />
       <Projects />
       <Skills />
+      <Contact />
       <Footer name='David Robison' date='2021' /> 
     </div>
   );
