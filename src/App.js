de443e8063811014a20ff02245213eb1d4ff292e
David Robison <david@robisonweb.dev>
2021-07-29 14:32:01 -0600

    Imported and added Projects component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './components/Nav';
 import Introduction from './components/Introduction';
 import About from './components/About';
+import Projects from './components/Projects';
 import Footer from './components/Footer';
 import './styles/App.css';
 
@@ -11,6 +12,7 @@ const App = () => {
       <Nav />
       <Introduction />
       <About />
+      <Projects />
       <Footer name='David Robison' date='2021' /> 
     </div>
   );
