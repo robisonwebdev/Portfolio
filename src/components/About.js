ee938dcf9fca175923f9b7a30cb4f4d4e19a1618
David Robison <david@robisonweb.dev>
2021-07-29 15:46:32 -0600

    Removed PageIntro import. Removed everything from about div and replaced with temp text

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import PageIntro from './PageIntro';
 import '../styles/About.css';
 
 const About = () => {
     return (
         <div className='about'>
-            <PageIntro description='Future text about me' title='About Me' />
-            <div>About me component here</div>
+            About me
         </div>
     );
 }
