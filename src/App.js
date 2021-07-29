8c35fd7072e1b96134f0bd1a1c7ea09e52ec8523
David Robison <david@robisonweb.dev>
2021-07-29 14:28:02 -0600

    Removed imports for react-dom and Content. Removed Components from App return for Router and content.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
 import Nav from './components/Nav';
-import Content from './components/Content';
 import Footer from './components/Footer';
 import './styles/App.css';
 
 const App = () => {
   return (
-    <Router>
-      <div id='App'>
-        <Nav />
-        <Content />
-        <Footer name='David Robison' date='2021' /> 
-      </div>
-    </Router>
+    <div id='App'>
+      <Nav />
+      <Footer name='David Robison' date='2021' /> 
+    </div>
   );
 }
 
