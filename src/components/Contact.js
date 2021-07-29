ffde4544f83d52aa925595516d365b73cf148e6e
David Robison <david@robisonweb.dev>
2021-07-29 15:48:02 -0600

    Remove PageIntro import and component.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import PageIntro from './PageIntro';
 import '../styles/Contact.css'
 
 const Contact = () => {
     return (
         <div className='contact'>
-            <PageIntro description='Future text for contact me' title='Contact Me' />
-            <div>Contact information componenet here</div>
+            Contact
         </div>
     );
 }
