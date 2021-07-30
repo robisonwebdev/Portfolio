8047cf265e7581d75c485e18418983d65e5df7a5
David Robison <david@robisonweb.dev>
2021-07-30 08:30:16 -0600

    Removed myPhoto and Button Component. Deleted everything within return and replaced with div, with an ID of introduction.

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import Button from './Button';
-import myPhoto from '../pictures/myPhoto.jpeg';
 import '../styles/Introduction.css';
 
 const Introduction = () => {
     return (
-        <div id='introContainer'>
-            <div id='introduction'>
-                <div id='introLeft'>
-                    <h1>Welcome, I'm David</h1>
-                    <p>Some text about me here</p>
-                    <Button text='My Work' className='introProjectsBtn' />
-                </div>
-                <div id='introRight'>
-                    <img id='introImage' src={myPhoto} />
-                </div>
-            </div>
-        </div>
+        <div id='introduction'></div>
     );
 }
 
