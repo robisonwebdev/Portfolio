2a3df6121eb6c0e50c1d5535746babdef2ba2cc9
David Robison <david@robisonweb.dev>
2021-07-30 08:39:04 -0600

    Created two divs within #introduction, with the ID's of introLeft and introRight. Inside #introLeft, added 3 h1's. Inside #introRight, added icon.

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,7 +3,16 @@ import '../styles/Introduction.css';
 
 const Introduction = () => {
     return (
-        <div id='introduction'></div>
+        <div id='introduction'>
+            <div id='introLeft'>
+                <h1>David Robison</h1>
+                <h1>Front-End</h1>
+                <h1>Web Developer</h1>
+            </div>
+            <div id='introRight'>
+                <i className='las la-laptop-code' />
+            </div>
+        </div>
     );
 }
 
