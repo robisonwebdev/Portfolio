be8713b4c06d22520daeaed3c9001c4549fde27e
David Robison <david@robisonweb.dev>
2021-07-30 09:10:39 -0600

    Destructered props. Added li, with a tag inside. Added props to href, and a tag text.

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const NavLink = () => {
-    return ();
+const NavLink = ({ address, text }) => {
+    return (
+        <li><a href={address}>{text}</a></li>
+    );
 }
 
 export default NavLink;
