977717ecc926be63516e5418be60292e48779378
David Robison <david@robisonweb.dev>
2021-07-30 08:55:51 -0600

    Removed introContainer.

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,15 +4,13 @@ import '../styles/Introduction.css';
 const Introduction = () => {
     return (
         <div id='introduction'>
-            <div id="introContainer">
-                <div id='introLeft'>
-                    <h1>David Robison</h1>
-                    <h1>Front-End</h1>
-                    <h1>Web Developer</h1>
-                </div>
-                <div id='introRight'>
-                    <i className='las la-laptop-code' />
-                </div>
+            <div id='introLeft'>
+                <h1>David Robison</h1>
+                <h1>Front-End</h1>
+                <h1>Web Developer</h1>
+            </div>
+            <div id='introRight'>
+                <i className='las la-laptop-code' />
             </div>
         </div>
     );
