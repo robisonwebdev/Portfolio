07166c7f112e348c21a08acc7b3a36290ae917b1
David Robison <david@robisonweb.dev>
2021-07-31 08:19:35 -0600

    Changed div from className to ID. Added an h1 tag and div tag with id of aboutMe, inside about div. Inside aboutMt, added two divs with an ID of myPicture and myInfo. Inside myInfo, added p tag and div with an ID of mySocialLinks.

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,15 @@ import '../styles/About.css';
 
 const About = () => {
     return (
-        <div className='about'>
-            About me
+        <div id='about'>
+            <h1>About Me</h1>
+            <div id='aboutMe'>
+                <div id='myPicture'></div>
+                <div id='myInfo'>
+                    <p></p>
+                    <div id='mySocialLinks'></div>
+                </div>
+            </div>
         </div>
     );
 }
