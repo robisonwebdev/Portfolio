ff1fd19d85fb033348fb0ab4783531874e1b6f6b
David Robison <david@robisonweb.dev>
2021-08-01 14:28:21 -0600

    Added h1 and div(#skillsList) to skills div

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,10 @@ import '../styles/Skills.css';
 
 const Skills = () => {
     return (
-        <div id='skills'>Skills</div>
+        <div id='skills'>
+            <h1>Skills</h1>
+            <div id='skillsList'></div>
+        </div>
     );
 }
 
