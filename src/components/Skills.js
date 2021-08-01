91d815574e613cd77f77a8afd9388ef05d49a846
David Robison <david@robisonweb.dev>
2021-08-01 14:32:39 -0600

    imported SkillCard

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import SkillCard from './SkillCard';
 import '../styles/Skills.css';
 
 const Skills = () => {
     return (
         <div id='skills'>
             <h1>Skills</h1>
-            <div id='skillsList'></div>
+            <div id='skillsList'>TEMP</div>
         </div>
     );
 }
