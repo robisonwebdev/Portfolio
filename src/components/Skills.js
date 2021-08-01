7ee4a37d7ddea70b9a63eea5057486cc42fe555d
David Robison <david@robisonweb.dev>
2021-08-01 14:50:33 -0600

    Added SkillCard for css, javascript, react, git, and terminal

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,6 +8,11 @@ const Skills = () => {
             <h1>Skills</h1>
             <div id='skillsList'>
                 <SkillCard className='lab la-html5' text='HTML5' />
+                <SkillCard className='lab la-css3-alt' text='CSS' />
+                <SkillCard className='lab la-js-square' text='JavaScript' />
+                <SkillCard className='lab la-react' text='React' />
+                <SkillCard className='lab la-git' text='Git' />
+                <SkillCard className='las la-terminal' text='Terminal' />
             </div>
         </div>
     );
