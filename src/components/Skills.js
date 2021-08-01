7c53777faeae613f18e2beca17fbfa69a5b56834
David Robison <david@robisonweb.dev>
2021-08-01 14:39:09 -0600

    Added SkillCard and inputted information for HTML5 into props

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,7 +6,9 @@ const Skills = () => {
     return (
         <div id='skills'>
             <h1>Skills</h1>
-            <div id='skillsList'>TEMP</div>
+            <div id='skillsList'>
+                <SkillCard className='lab la-html5' text='HTML5' />
+            </div>
         </div>
     );
 }
