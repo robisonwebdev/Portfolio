0112b7a601a6b2fe3a1ac3d36a182fd5d8dea4a8
David Robison <david@robisonweb.dev>
2021-08-02 11:53:12 -0600

    Added live and source address for each project.

--- src/projectInformation.js
+++ src/projectInformation.js
@@ -5,28 +5,32 @@ import todoApp from './pictures/todoApp.png';
 
 export const projectInformation = [
     {
-        'address': 'https://robisonwebdev.github.io/shopping-cart/',
+        'live': 'https://robisonwebdev.github.io/shopping-cart/',
+        'source': 'https://github.com/robisonwebdev/shopping-cart',
         'details': 'My objective for creating a Shopping Cart with react was to learn how to use Router. Router allowed me move from creating single page applications, to multiple page applications. With Router I was able to setup a navigation using HashRouter, Switch, Route, and Link.',
         'image': shoppingCart,
         'imageAlt': 'Shopping cart app',
         'title': 'Shopping Cart App'
     },
     {
-        'address': 'https://robisonwebdev.github.io/memory-card-game/',
+        'live': 'https://robisonwebdev.github.io/memory-card-game/',
+        'source': 'https://github.com/robisonwebdev/memory-card-game',
         'details': 'Memory Card Game',
         'image': memoryCard,
         'imageAlt': 'Stargate Memory Card Game',
         'title': 'Stargate Memory Card Game'
     },
     {
-        'address': 'https://robisonwebdev.github.io/Restaurant-Page/',
+        'live': 'https://robisonwebdev.github.io/Restaurant-Page/',
+        'source': 'https://github.com/robisonwebdev/Restaurant-Page',
         'details': 'Restaurant page for a mock-up restaurant called Red Bresaola',
         'image': restaurantPage,
         'imageAlt': 'Image of restaurant mock-up restaurant called Red Bresaola',
         'title': 'Red Bresaola'
     },
     {
-        'address': 'https://robisonwebdev.github.io/Todo-List/',
+        'live': 'https://robisonwebdev.github.io/Todo-List/',
+        'source': 'https://github.com/robisonwebdev/Todo-List',
         'details': 'Todo-List program is to create a simple todo list, using mostly JavaScript',
         'image': todoApp,
         'imageAlt': 'Image of todo app',
