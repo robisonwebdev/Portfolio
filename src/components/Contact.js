7699eb23bb6ed5dc291d6304f80335281ea51c83
David Robison <david@robisonweb.dev>
2021-08-04 11:01:42 -0600

    Gave div an ID of contactDetails. Created another div and gave an ID of contactLinks"

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,11 +5,12 @@ const Contact = () => {
     return (
         <div id='contact'>
             <h1>Contact Me</h1>
-            <div>
+            <div id='contactDetails'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget lorem leo. Cras lectus elit, semper at urna quis, tincidunt egestas ex.</p>
                 <p>Nullam vitae tempus dui, quis ornare dolor. Nulla iaculis enim at urna egestas, id suscipit est rutrum. Pellentesque consectetur ultricies mauris, sit amet lobortis ante sagittis sed.</p>
                 <p>My inbox:<br /> DavidRBRobison@gmail.com</p>
             </div>
+            <div id='contactLinks'></div>
         </div>
     );
 }
