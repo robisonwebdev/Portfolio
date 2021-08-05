176f49e9679e468771db5eb23cf1e0116cfef7be
David Robison <david@robisonweb.dev>
2021-08-05 12:59:09 -0600

    Changed Contact Me h1, to Get in touch

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,11 +5,11 @@ import '../styles/Contact.css'
 const Contact = () => {
     return (
         <div id='contact'>
-            <h1>Contact Me</h1>
+            <h1>Get in touch</h1>
             <div id='contactDetails'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget lorem leo. Cras lectus elit, semper at urna quis, tincidunt egestas ex.</p>
+                <p></p>
                 <p>Nullam vitae tempus dui, quis ornare dolor. Nulla iaculis enim at urna egestas, id suscipit est rutrum. Pellentesque consectetur ultricies mauris, sit amet lobortis ante sagittis sed.</p>
-                <p>My inbox:<br /> DavidRBRobison@gmail.com</p>
+                <p>You can contact me by my email:<br /> DavidRBRobison@gmail.com</p>
             </div>
             <div id='contactLinks'>
                 <ContactLink address='https://github.com/robisonwebdev' iconClass='lab la-github' />
