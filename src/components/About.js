3974618f89c723f0f26d0be487444220bed49dcc
David Robison <david@robisonweb.dev>
2021-08-05 13:04:19 -0600

    Changed mySocialLinks from ID to class

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ const About = () => {
                         <p>Exercitation exercitation exercitation officia magna dolore cupidatat nulla. Mollit sunt irure proident laborum et dolore dolore elit nisi enim commodo enim. Ex officia in id veniam id enim irure tempor do anim occaecat reprehenderit minim. Irure sit esse sunt dolore consectetur ad dolore culpa minim cillum duis qui sint cillum. Laboris ad anim consequat esse aliquip non incididunt officia velit eu aliqua excepteur. Occaecat consequat aliquip do ipsum est cupidatat ullamco nisi esse ad esse veniam. Consectetur nulla aliqua nostrud excepteur cupidatat amet sunt ut irure laborum cupidatat excepteur cupidatat.</p>
                         <p>Veniam incididunt voluptate eiusmod ullamco laboris aliqua esse cillum et deserunt minim duis. Excepteur non quis commodo amet sint ut. Nostrud deserunt culpa non aliqua tempor deserunt sunt nisi pariatur sunt commodo et. Excepteur fugiat laboris aliqua labore proident dolore nulla eiusmod. Tempor ut velit enim cillum.</p>
                     </div>
-                    <div id='mySocialLinks'>
+                    <div className='mySocialLinks'>
                         <ContactLink address='https://github.com/robisonwebdev' iconClass='lab la-github' />
                         <ContactLink address='https://www.linkedin.com/in/davidrulonrobison/' iconClass='lab la-linkedin' />
                     </div>
