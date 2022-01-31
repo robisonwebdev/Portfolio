a79fadd6df07f43731205666c4725687ff91213e
robisonwebdev <david@robisonweb.dev>
2022-01-31 14:36:50 -0700

    Removed all code. Imported React, Navigation, Main, and Footer. Setup initial functional App component. Added Navigation, Main, and Footer components to return div.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import Nav from './components/Nav';
-import Introduction from './components/Introduction';
-import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import './styles/App.css';
+import Navigation from './components/navigation/Navigation';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
 
 const App = () => {
-  return (
-    <div id='App'>
-      <Nav />
-      <Introduction />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-      <Footer name='David Robison' date='2021' /> 
-    </div>
-  );
-}
+    return (
+      <div id='App'>
+        <Navigation />
+        <Main />
+        <Footer />
+      </div>
+    );
+};
 
 export default App;
