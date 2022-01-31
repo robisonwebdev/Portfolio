f3e6cf531cba3dc3a4a0937fe6ea87e9cbcca80b
robisonwebdev <david@robisonweb.dev>
2022-01-31 14:32:27 -0700

    Imported Footer.css Gave return footer a className of footer.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../../styles/Footer/Footer.css';
 
 const Footer = () => {
     return (
-        <footer></footer>
+        <footer className='footer'></footer>
     );
 };
 
