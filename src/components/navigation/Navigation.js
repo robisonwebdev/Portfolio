6612542364bd70857ce1aed6dfa882197c19764a
robisonwebdev <david@robisonweb.dev>
2022-02-02 14:28:09 -0700

    Moved everything inside of return nav, into a div with className of .navigation_links. Changed ul className to .navlinks

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -6,12 +6,14 @@ import '../../styles/Navigation/Navigation.css';
 const Navigation = () => {
     return (
         <nav className='navigation'>
-            <Title address='#home' title='David Robison' />
-            <ul className='navigation_links'>
-                <Navlink address='#projects' text='Projects' />
-                <Navlink address='#skills' text='Skills' />
-                <Navlink address='#contact' text='Contact' />
-            </ul>
+            <div className='.navigation_links'>
+                <Title address='#home' title='David Robison' />
+                <ul className='.navlinks'>
+                    <Navlink address='#projects' text='Projects' />
+                    <Navlink address='#skills' text='Skills' />
+                    <Navlink address='#contact' text='Contact' />
+                </ul>
+            </div>
         </nav>
     );
 };
