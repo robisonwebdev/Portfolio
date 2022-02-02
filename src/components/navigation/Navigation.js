08b3722d72451e773ba6b897d4158e066ae2578a
robisonwebdev <david@robisonweb.dev>
2022-02-02 14:12:32 -0700

    Imported Title component. Called Title component in return nav.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './Title';
 import '../../styles/Navigation/Navigation.css';
 
 const Navigation = () => {
     return (
         <nav className='navigation'>
-
+            <Title title='David Robison' />
         </nav>
     );
 };
