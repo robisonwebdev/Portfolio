0afa308712800d6cd5d002831b3f4ee78b3b8267
robisonwebdev <david@robisonweb.dev>
2022-02-02 14:21:02 -0700

    Imported Navlink component. Added ul with className of .navigation_links to return nav. Inside of .navigation_links called Navlink 3 times.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Navlink from './Navlink';
 import Title from './Title';
 import '../../styles/Navigation/Navigation.css';
 
 const Navigation = () => {
     return (
         <nav className='navigation'>
-            <Title title='David Robison' />
+            <Title address='#home' title='David Robison' />
+            <ul className='navigation_links'>
+                <Navlink address='#projects' text='Projects' />
+                <Navlink address='#skills' text='Skills' />
+                <Navlink address='#contact' text='Contact' />
+            </ul>
         </nav>
     );
 };
