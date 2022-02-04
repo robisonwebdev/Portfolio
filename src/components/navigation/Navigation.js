a219cba44bdea9066fa9f27e7a74dbe731ad6f98
robisonwebdev <david@robisonweb.dev>
2022-02-04 14:27:00 -0700

    Imported useState. Setup state for showLinks, and set to an initial value of false. Moved Title and MobileMenu components into new div with className of .navigation_bar. Moved ul and Navlink's into a ternery set to showLinks.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MobileMenu from './MobileMenu';
 import Navlink from './Navlink';
 import Title from './Title';
 import '../../styles/Navigation/Navigation.css';
 
 const Navigation = () => {
+    const [showLinks, setShowLinks] = useState(false);
+
+    const handleMobileMenu = () => {
+        setShowLinks(!showLinks);
+    }
+
     return (
         <nav className='navigation'>
-            <div className='navigation_links'>
+            <div className='navigation_bar'>
                 <Title address='#home' title='David Robison' />
-                <ul>
-                    <Navlink address='#projects' text='Projects' />
-                    <Navlink address='#skills' text='Skills' />
-                    <Navlink address='#contact' text='Contact' />
-                </ul>
-                <MobileMenu />
+                <MobileMenu onClick={handleMobileMenu} />
+            </div>            
+            <div className='navigation_links'>
+                {showLinks 
+                    ?   <ul>
+                            <Navlink address='#projects' text='Projects' />
+                            <Navlink address='#skills' text='Skills' />
+                            <Navlink address='#contact' text='Contact' />
+                        </ul>
+                    : null
+                }
             </div>
         </nav>
     );
