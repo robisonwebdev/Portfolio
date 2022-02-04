3046ad25501b9f9ed6ac8a73d470a7ed70876b66
robisonwebdev <david@robisonweb.dev>
2022-02-04 12:50:27 -0700

    Imported MobileMenu.css Added icon to button.

--- src/components/navigation/MobileMenu.js
+++ src/components/navigation/MobileMenu.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import '../../styles/Navigation/MobileMenu.css';
 
 const MobileMenu = () => {
     return (
-        <button></button>
+        <button>
+            <i className='las la-bars' />
+        </button>
     );
 };
 
