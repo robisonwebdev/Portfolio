e6f48d6f65ae42caa91cb8d994d7ae7708585e36
robisonwebdev <david@robisonweb.dev>
2022-02-05 12:34:33 -0700

    Imported DesktopMenu.css Changed NavMenu to DesktopMenu. Changed className to desktop_menu

--- src/components/navigation/NavMenu.js
+++ src/components/navigation/NavMenu.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Navlink from './Navlink';
+import '../../styles/Navigation/DesktopMenu.css';
 
-const NavMenu = () => {
+const DesktopMenu = () => {
     return (
-        <ul className='navigation_links'>
+        <ul className='desktop_menu'>
             <Navlink address='#projects' text='Projects' />
             <Navlink address='#skills' text='Skills' />
             <Navlink address='#contact' text='Contact' />
