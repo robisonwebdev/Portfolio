2235f83376d2c0599db167616fd74041eb5c0a78
robisonwebdev <david@robisonweb.dev>
2022-02-05 12:52:33 -0700

    Set state for width, and set to an initial value of window.innerWidth Setup breakpoint variable. Setup useEffect to handle window resizing. Moved DesktopMenu inside a ternery set to width. Moved MobileMenuButton inside a ternery set to width.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DesktopMenu from './DesktopMenu';
 import MobileMenu from './MobileMenu';
 import MobileMenuButton from './MobileMenuButton';
@@ -7,17 +7,29 @@ import '../../styles/Navigation/Navigation.css';
 
 const Navigation = () => {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth);
+    const breakpoint = 700;
 
     const handleMobileMenuButton = () => {
         setShowMobileMenu(!showMobileMenu);
-    }
+    };
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => window.removeEventListener('resize', handleWindowResize);
+    }, []);
 
     return (
         <nav className='navigation'>
             <div className='navigation_bar'>
                 <Title address='#home' title='David Robison' />
-                <DesktopMenu />
-                <MobileMenuButton onClick={handleMobileMenuButton} />
+                {width < breakpoint ? null : <DesktopMenu />}
+                {width < breakpoint ? <MobileMenuButton onClick={handleMobileMenuButton} /> : null}
             </div>            
             {showMobileMenu ? <MobileMenu /> : null}
         </nav>
