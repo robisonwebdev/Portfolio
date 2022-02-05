e4412bb16b30689b08f8d62c344af6e1f378ff0a
robisonwebdev <david@robisonweb.dev>
2022-02-05 12:40:12 -0700

    Imported DesktopMenu and MobileMenuButton components. Removed Navlink component. Changed handleMobileMenu to handleMobileMenuButton. Added DesktopMenu component to .navigation_bar Removed ul from .navigation_mobile_links, and replaced with MobileMenu component.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
+import DesktopMenu from './DesktopMenu';
 import MobileMenu from './MobileMenu';
-import Navlink from './Navlink';
+import MobileMenuButton from './MobileMenuButton';
 import Title from './Title';
 import '../../styles/Navigation/Navigation.css';
 
 const Navigation = () => {
     const [showLinks, setShowLinks] = useState(false);
 
-    const handleMobileMenu = () => {
+    const handleMobileMenuButton = () => {
         setShowLinks(!showLinks);
     }
 
@@ -15,17 +16,11 @@ const Navigation = () => {
         <nav className='navigation'>
             <div className='navigation_bar'>
                 <Title address='#home' title='David Robison' />
-                <MobileMenu onClick={handleMobileMenu} />
+                <MobileMenuButton onClick={handleMobileMenuButton} />
+                <DesktopMenu />
             </div>            
             <div className='navigation_mobile_links'>
-                {showLinks 
-                    ?   <ul>
-                            <Navlink address='#projects' text='Projects' />
-                            <Navlink address='#skills' text='Skills' />
-                            <Navlink address='#contact' text='Contact' />
-                        </ul>
-                    : null
-                }
+                {showLinks ? <MobileMenu /> : null}
             </div>
         </nav>
     );
