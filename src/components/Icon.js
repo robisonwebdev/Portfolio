b7b85414e6d6a47d785d42500f26adb7e4848db3
robisonwebdev <david@robisonweb.dev>
2022-02-06 10:36:45 -0700

    Added noreferrer to a tag.

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,7 +3,7 @@ import '../styles/Icon.css';
 
 const Icon = ({ address, icon }) => {
     return (
-        <a className='icon' href={address} target='_blank'>
+        <a className='icon' href={address} rel="noreferrer" target='_blank'>
             <i className={icon} />
         </a>
     );
