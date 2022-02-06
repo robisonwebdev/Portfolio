46435aca5ce471481384e3e74cc92a9cdf492e94
robisonwebdev <david@robisonweb.dev>
2022-02-06 10:36:05 -0700

    Imported Icon.js Replaced i tag's with Icon component.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Icon from '../Icon';
 import '../../styles/Footer/Footer.css';
 
 const Footer = () => {
     return (
         <footer className='footer'>
             <div className='footer_links'>
-                <i className='lab la-github' />
-                <i className='lab la-linkedin' />
-                <i className='las la-envelope' />
+                <Icon address='https://github.com/robisonwebdev' icon='lab la-github'/>
+                <Icon address='https://www.linkedin.com/in/davidrulonrobison/' icon='lab la-linkedin' />
+                <Icon address='mailto:david@robisonweb.dev' icon='las la-envelope' />
             </div>
             <div className='footer_copyright'>
                 <p>David Robison <span>&#169;2022</span></p>
