82273926c7e9060128e1d7ed169b1d0a83c3500c
robisonwebdev <david@robisonweb.dev>
2022-02-06 10:08:58 -0700

    Add text to .footer_copyright

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
     return (
         <footer className='footer'>
             <div className='footer_links'></div>
-            <div className='footer_copyright'></div>
+            <div className='footer_copyright'>
+                <p>David Robison <span>&#169;2022</span></p>
+            </div>
         </footer>
     );
 };
