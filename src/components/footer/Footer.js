01c4176cd652fd9120f9f22bda8479063b4c53b7
robisonwebdev <david@robisonweb.dev>
2022-02-06 10:11:39 -0700

    Added icons for github, linkedin, and mail to .footer_links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,11 @@ import '../../styles/Footer/Footer.css';
 const Footer = () => {
     return (
         <footer className='footer'>
-            <div className='footer_links'></div>
+            <div className='footer_links'>
+                <i className='lab la-github' />
+                <i className='lab la-linkedin' />
+                <i className='las la-envelope' />
+            </div>
             <div className='footer_copyright'>
                 <p>David Robison <span>&#169;2022</span></p>
             </div>
