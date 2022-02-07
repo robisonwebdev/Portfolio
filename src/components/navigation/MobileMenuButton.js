4a4372c4527753ef7d1153c351282e15d5c84021
robisonwebdev <david@robisonweb.dev>
2022-02-07 09:12:51 -0700

    Destructured icon prop. Setup ternary on i tag's className, to deteremine which icon to show.

--- src/components/navigation/MobileMenuButton.js
+++ src/components/navigation/MobileMenuButton.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../../styles/Navigation/MobileMenuButton.css';
 
-const MobileMenuButton = ({ onClick }) => {
+const MobileMenuButton = ({ icon, onClick }) => {
     return (
         <button className='mobile_menu_button' onClick={onClick}>
-            <i className='las la-bars' />
+            <i className={icon ? 'las la-times' : 'las la-bars'} />
         </button>
     );
 };
