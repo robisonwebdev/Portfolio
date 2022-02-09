f2a223c3e5ebb92ec890a08603ea5cbd2022a833
robisonwebdev <david@robisonweb.dev>
2022-02-09 10:34:43 -0700

    Added div with className of welcome_message to return section. Inside .welcome_message, added 2 h1's with text. Added button to .welcome

--- src/components/main/Welcome.js
+++ src/components/main/Welcome.js
@@ -4,7 +4,11 @@ import '../../styles/Main/Welcome.css';
 const Welcome = () => {
     return (
         <section className='welcome'>
-            welcome
+            <div className='welcome_message'>
+                <h1>Welcome, I'm <span>David Robison</span>.</h1>
+                <h1>I'm a front-end web developer.</h1>
+            </div>
+            <button>More Information <i className='las la-chevron-down' /></button>
         </section>
     );
 };
