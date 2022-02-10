3610b2b46e7ea426aeb1e63b220bf5954d3b33bb
robisonwebdev <david@robisonweb.dev>
2022-02-10 09:29:43 -0700

    Destructured handleNavClick and passed it to Navlink components.

--- src/components/navigation/MobileMenu.js
+++ src/components/navigation/MobileMenu.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Navlink from './Navlink';
 import '../../styles/Navigation/MobileMenu.css';
 
-const MobileMenu = () => {
+const MobileMenu = ({ handleNavClick }) => {
     return (
         <div className='navigation_mobile_links'>
             <ul>
-                <Navlink address='#about' text='About' />
-                <Navlink address='#projects' text='Projects' />
-                <Navlink address='#contact' text='Contact' />
+                <Navlink address='#about' handleNavClick={handleNavClick} text='About' />
+                <Navlink address='#projects' handleNavClick={handleNavClick} text='Projects' />
+                <Navlink address='#contact' handleNavClick={handleNavClick} text='Contact' />
             </ul>
         </div>
     );
