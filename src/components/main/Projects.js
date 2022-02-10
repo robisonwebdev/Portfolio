b2046592ca19f0af0db4e98e6bb2b6968c0e6177
robisonwebdev <david@robisonweb.dev>
2022-02-10 09:33:23 -0700

    Imported Projects.css

--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import '../../styles/Main/Projects.css';
 
 const Projects = () => {
     return (
-        <section className='projects' id='projects'></section>
+        <section className='projects' id='projects'>
+            Projects
+        </section>
     );
 };
 
