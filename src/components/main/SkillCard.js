8cbfd47796209b435eca4008332c56db175bdb2b
robisonwebdev <david@robisonweb.dev>
2022-02-11 11:50:44 -0700

    Destructured icon and name prop. Gave return div a className of skill_card Setup i and h4 tag inside skill_card Set i tag's className to icon prop. Set h4 inner text to name prop.

--- src/components/main/SkillCard.js
+++ src/components/main/SkillCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const SkillCard = () => {
-  return (
-    <div>Skill Card</div>
-  )
+const SkillCard = ({ icon, name }) => {
+    return (
+      <div className='skill_card'>
+        <i className={icon} />
+        <h4>{name}</h4>
+      </div>
+    )
 };
 
 export default SkillCard;
