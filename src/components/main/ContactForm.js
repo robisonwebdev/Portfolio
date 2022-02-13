7ba9e4f61cf16264575ab62f98715fa8cbb1bb68
robisonwebdev <david@robisonweb.dev>
2022-02-13 09:18:39 -0700

    Imported useForm, ValidationError, and ContactForm.css Copied and pasted form code from formspree.

--- src/components/main/ContactForm.js
+++ src/components/main/ContactForm.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+import '../../styles/Main/ContactForm.css';
 
 const ContactForm = () => {
+    const [state, handleSubmit] = useForm("xvolagzn");
+
+    if (state.succeeded) {
+        return <p>Thanks for joining!</p>;
+    }
+
     return (
-        <form></form>
-    )
+        <section className='contact_form'>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">
+                    Email Address
+                </label>
+                <input
+                    id="email"
+                    type="email" 
+                    name="email"
+                />
+                <ValidationError 
+                    prefix="Email" 
+                    field="email"
+                    errors={state.errors}
+                />
+                <textarea
+                    id="message"
+                    name="message"
+                />
+                <ValidationError 
+                    prefix="Message" 
+                    field="message"
+                    errors={state.errors}
+                />
+                <button type="submit" disabled={state.submitting}>
+                    Submit
+                </button>
+            </form>
+        </section>
+    );
 };
 
 export default ContactForm;
