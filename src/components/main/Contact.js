11e9a1e13fc8848b92b41f341d6901dc2abbfef7
robisonwebdev <david@robisonweb.dev>
2022-02-13 09:11:47 -0700

    Imported ContactForm component. Added ContactForm componenet to .contact_form

--- src/components/main/Contact.js
+++ src/components/main/Contact.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ContactForm from './ContactForm';
 import '../../styles/Main/Contact.css';
 
 const Contact = () => {
     return (
         <section className='contact' id='contact'>
             <h1>Contact</h1>
-            <section className='contact_form'></section>
+            <section className='contact_form'>
+                <ContactForm />
+            </section>
         </section>
     );
 };
