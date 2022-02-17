34e00bfdf75542ade666f3d60eedebd4c3695794
robisonwebdev <david@robisonweb.dev>
2022-02-17 11:50:33 -0700

    Added SkillCard component for React, JS, Git, Github, Terminal, Html, and css.

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -9,9 +9,19 @@ const About = () => {
             <section className='about_information'>
                 <section className='about_me'></section>
                 <section className='skills'>
-                    <div className='skills_icons'></div>
-                    <div className='skills_icons'></div>
-                    <div className='skills_icons'></div>
+                    <div className='skills_icons'>
+                        <SkillCard icon='lab la-react' name='ReactJS' />
+                        <SkillCard icon='lab la-js-square' name='JavaScript' />
+                    </div>
+                    <div className='skills_icons'>
+                        <SkillCard icon='lab la-git' name='Git' />
+                        <SkillCard icon='lab la-github' name='Github' />
+                        <SkillCard icon='las la-terminal' name='Terminal' />
+                    </div>
+                    <div className='skills_icons'>
+                        <SkillCard icon='lab la-html5' name='HTML5' />
+                        <SkillCard icon='lab la-css3-alt' name='CSS3' />
+                    </div>
                 </section>
             </section>
         </section>
