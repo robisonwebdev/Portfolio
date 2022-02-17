fe1ddcd2cb389fcbf599e75d247b9920eeda7de6
robisonwebdev <david@robisonweb.dev>
2022-02-17 12:39:34 -0700

    Removed div's from .skill section

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -9,19 +9,13 @@ const About = () => {
             <section className='about_information'>
                 <section className='about_me'></section>
                 <section className='skills'>
-                    <div className='skill_cards'>
-                        <SkillCard icon='lab la-react' name='ReactJS' />
-                        <SkillCard icon='lab la-js-square' name='JavaScript' />
-                    </div>
-                    <div className='skill_cards'>
-                        <SkillCard icon='lab la-git' name='Git' />
-                        <SkillCard icon='lab la-github' name='Github' />
-                        <SkillCard icon='las la-terminal' name='Terminal' />
-                    </div>
-                    <div className='skill_cards'>
-                        <SkillCard icon='lab la-html5' name='HTML5' />
-                        <SkillCard icon='lab la-css3-alt' name='CSS3' />
-                    </div>
+                    <SkillCard icon='lab la-react' name='ReactJS' />
+                    <SkillCard icon='lab la-js-square' name='JavaScript' />                    
+                    <SkillCard icon='lab la-git' name='Git' />
+                    <SkillCard icon='lab la-github' name='Github' />
+                    <SkillCard icon='las la-terminal' name='Terminal' />                    
+                    <SkillCard icon='lab la-html5' name='HTML5' />
+                    <SkillCard icon='lab la-css3-alt' name='CSS3' />                    
                 </section>
             </section>
         </section>
