2715763dc2314bba941961acb627607211f9023e
robisonwebdev <david@robisonweb.dev>
2022-02-18 09:08:48 -0700

    Imported beard.png as avater. Added img tag to .about_me, passing it avater to src. Removed github SkillCard.

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SkillCard from './SkillCard';
+import avatar from '../../pictures/beard.png';
 import '../../styles/Main/About.css';
 
 const About = () => {
@@ -7,12 +8,13 @@ const About = () => {
         <section className='about' id='about'>
             <h1>About</h1>
             <section className='about_information'>
-                <section className='about_me'></section>
+                <section className='about_me'>
+                    <img src={avatar} alt='Beaded avater' />
+                </section>
                 <section className='skills'>
                     <SkillCard icon='lab la-react' name='ReactJS' />
                     <SkillCard icon='lab la-js-square' name='JavaScript' />                    
                     <SkillCard icon='lab la-git' name='Git' />
-                    <SkillCard icon='lab la-github' name='Github' />
                     <SkillCard icon='las la-terminal' name='Terminal' />                    
                     <SkillCard icon='lab la-html5' name='HTML5' />
                     <SkillCard icon='lab la-css3-alt' name='CSS3' />                    
