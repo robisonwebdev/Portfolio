b26352ad5bc155888ec41c54f64fc05d5f524a91
robisonwebdev <david@robisonweb.dev>
2022-02-18 13:33:50 -0700

    Changed .about_me section to an article tag.

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -8,9 +8,10 @@ const About = () => {
         <section className='about' id='about'>
             <h1>About</h1>
             <section className='about_information'>
-                <section className='about_me'>
+                <article className='about_me'>
                     <img src={avatar} alt='Beaded avater' />
-                </section>
+                    <p>Ut odio velit, feugiat vitae rhoncus in, tincidunt eu arcu. Nulla ac elementum nunc. Integer laoreet in turpis ac mattis. Integer sodales sapien eu dictum ullamcorper. Nam varius non est non posuere. Mauris non porta mauris. Praesent finibus sagittis suscipit. Proin eget lobortis tellus. Ut eleifend odio eget venenatis suscipit. Suspendisse consequat ullamcorper metus nec lobortis. Morbi a leo efficitur orci lobortis luctus nec ac neque. Cras vel elementum nisl. Vivamus lacinia sit amet libero a cursus. Curabitur aliquet nisi nunc, vel tristique nulla ultricies eget. Cras varius enim lacus, sed tempor libero ornare ac.</p>
+                </article>
                 <section className='skills'>
                     <SkillCard icon='lab la-react' name='ReactJS' />
                     <SkillCard icon='lab la-js-square' name='JavaScript' />                    
