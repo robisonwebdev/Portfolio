4ab70d77e1d71af94f08fc9ee7cd3488756212c1
robisonwebdev <david@robisonweb.dev>
2022-04-06 09:31:04 -0600

    Updated projectInformation.

--- src/projectInformation.js
+++ src/projectInformation.js
@@ -1,5 +1,8 @@
+import calculator from './pictures/calculator.png';
+import cvBuilder from './pictures/cvBuilder.png';
+import lotrQuote from './pictures/lotrQuote.png';
 import shoppingCart from './pictures/shoppingCart.png';
-import memoryCard from './pictures/memoryCardGame.png';
+import ticTacToe from './pictures/ticTacToe.png'
 import todoList from './pictures/todoList.png';
 
 export const projectInformation = [
@@ -8,22 +11,22 @@ export const projectInformation = [
         'source': 'https://github.com/robisonwebdev/shopping-cart',
         'details': 'My objective for creating a Shopping Cart with react was to learn how to use Router. Router allowed me move from creating single page applications, to multiple page applications. With Router I was able to setup a navigation using HashRouter, Switch, Route, and Link.',
         'image': shoppingCart,
-        'imageAlt': 'Shopping cart app',
+        'imageAlt': 'Shopping cart app.',
         'title': 'Shopping Cart App'
     },
     {
-        'live': 'https://robisonwebdev.github.io/memory-card-game/',
-        'source': 'https://github.com/robisonwebdev/memory-card-game',
-        'details': 'Memory Card Game',
-        'image': memoryCard,
-        'imageAlt': 'Stargate Memory Card Game',
-        'title': 'Stargate Memory Card Game'
+        'live': 'https://robisonwebdev.github.io/calculator-react/',
+        'source': 'https://github.com/robisonwebdev/calculator-react',
+        'details': 'Calculator application created using React.',
+        'image': calculator,
+        'imageAlt': 'Screenshot of Calculator application.',
+        'title': 'Calculator App'
     },
     {
         'live': 'https://robisonwebdev.github.io/tic-tac-toe-react/',
         'source': 'https://github.com/robisonwebdev/tic-tac-toe-react',
         'details': 'Tic-Tac-Toe game created in React.js. Currently only playable between two users.',
-        'image': null,
+        'image': ticTacToe,
         'imageAlt': 'Screenshot of Tic-Tac-Toe game.',
         'title': 'Tic-Tac-Toe'
     },
@@ -39,8 +42,16 @@ export const projectInformation = [
         'live': 'https://robisonwebdev.github.io/cv-application/',
         'source': 'https://github.com/robisonwebdev/cv-application',
         'details': 'Basic CV application created in React.js, allowing user to create a simple resume.',
-        'image': null,
+        'image': cvBuilder,
         'imageAlt': 'Screenshot of CV application.',
         'title': 'CV Builder'
+    },
+    {
+        'live': 'https://robisonwebdev.github.io/lotr-api-project/',
+        'source': 'https://github.com/robisonwebdev/lotr-api-project',
+        'details': 'Quote generator using react and Lord of the Rings api.',
+        'image': lotrQuote,
+        'imageAlt': 'Screenshot of Lord of the Rings quote generator.',
+        'title': 'Lord of the Rings Quote Generator'
     }
-]
+];
