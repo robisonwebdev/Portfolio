ae9d4fd359d4f43eaf5fa169a60fc5a61003221f
robisonwebdev <david@robisonweb.dev>
2022-04-07 12:19:04 -0600

    Added project to component props. Destrucutred image and imageAlt from project. Added img element to .project_card_image, passing image to src and imageAlt to alt.

--- src/components/Main/ProjectCard.js
+++ src/components/Main/ProjectCard.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const ProjectCard = () => {
+const ProjectCard = ({ project }) => {
+    const { image, imageAlt } = project;
+
     return (
         <section className='project_card'>
-            <section className='project_card_image'></section>
+            <section className='project_card_image'>
+                <img src={image} alt={imageAlt} />
+            </section>
             <section className='project_card_information'></section>
         </section>
     );
