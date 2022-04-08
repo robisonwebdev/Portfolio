e37ede210a6a3b0f119b7c984c5618931d9b0aab
robisonwebdev <david@robisonweb.dev>
2022-04-08 16:26:20 -0600

    Imported ProjectCard component and projectInformation.js Setup mapProjects variable to map over projectInformation and return ProjectCard. Changed .project_list to .projects_list Called mapProjects in .projects_list

--- src/components/Main/Projects.js
+++ src/components/Main/Projects.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import ProjectCard from './ProjectCard';
+import projectInformation from '../../projectInformation';
 import '../../styles/Main/Projects.css';
 
 const Projects = () => {
+    const mapProjects = projectInformation.map(project => {
+        return <ProjectCard key={project.id} project={project} />
+    })
+
     return (
         <section className='projects' id='projects'>
             <h1>Projects</h1>
-            <section className='project_list'></section>
+            <section className='projects_list'>
+                {mapProjects}
+            </section>
         </section>
     );
 };
