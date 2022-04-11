15dae4f299ed5659d331f2b2be0f9f23bdbbb8f1
robisonwebdev <david@robisonweb.dev>
2022-04-11 08:57:08 -0600

    Changed details on shopping cart.

--- src/projectInformation.js
+++ src/projectInformation.js
@@ -9,7 +9,7 @@ export const projectInformation = [
     {
         'live': 'https://robisonwebdev.github.io/shopping-cart/',
         'source': 'https://github.com/robisonwebdev/shopping-cart',
-        'details': 'My objective for creating a Shopping Cart with react was to learn how to use Router. Router allowed me move from creating single page applications, to multiple page applications. With Router I was able to setup a navigation using HashRouter, Switch, Route, and Link.',
+        'details': 'A product landing page created in ReactJS.',
         'image': shoppingCart,
         'imageAlt': 'Shopping cart app.',
         'title': 'Shopping Cart App',
