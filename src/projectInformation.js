f8eeb78c3a7091f06f29d75f1b30c584743ef6f8
robisonwebdev <david@robisonweb.dev>
2022-04-20 07:39:09 -0600

    Updated the details in each object.

--- src/projectInformation.js
+++ src/projectInformation.js
@@ -9,7 +9,7 @@ export const projectInformation = [
     {
         'live': 'https://robisonwebdev.github.io/shopping-cart/',
         'source': 'https://github.com/robisonwebdev/shopping-cart',
-        'details': 'A product landing page created in ReactJS.',
+        'details': 'A mockup of an online store built using ReactJS.',
         'image': shoppingCart,
         'imageAlt': 'Shopping cart app.',
         'title': 'Shopping Cart App',
@@ -18,7 +18,7 @@ export const projectInformation = [
     {
         'live': 'https://robisonwebdev.github.io/todo-react/',
         'source': 'https://github.com/robisonwebdev/todo-react',
-        'details': 'Todo List a task management application created in React.js. ',
+        'details': 'Todo List is a task management application created in React.js. ',
         'image': todoList,
         'imageAlt': 'Screenshot of Todo List application.',
         'title': 'Todo List',
@@ -36,7 +36,7 @@ export const projectInformation = [
     {
         'live': 'https://robisonwebdev.github.io/cv-application/',
         'source': 'https://github.com/robisonwebdev/cv-application',
-        'details': 'Basic CV application created in React.js, allowing user to create a simple resume.',
+        'details': 'Basic CV application created in ReactJS, allowing user to create a simple resume.',
         'image': cvBuilder,
         'imageAlt': 'Screenshot of CV application.',
         'title': 'CV Builder',
@@ -45,7 +45,7 @@ export const projectInformation = [
     {
         'live': 'https://robisonwebdev.github.io/lotr-api-project/',
         'source': 'https://github.com/robisonwebdev/lotr-api-project',
-        'details': 'Quote generator using react and Lord of the Rings api.',
+        'details': 'Quote generator using ReactJS and Lord of the Rings api.',
         'image': lotrQuote,
         'imageAlt': 'Screenshot of Lord of the Rings quote generator.',
         'title': 'Quote Generator',
@@ -54,7 +54,7 @@ export const projectInformation = [
     {
         'live': 'https://robisonwebdev.github.io/tic-tac-toe-react/',
         'source': 'https://github.com/robisonwebdev/tic-tac-toe-react',
-        'details': 'Tic-Tac-Toe game created in React.js. Currently only playable between two users.',
+        'details': 'Tic-Tac-Toe game created in ReactJS, playable between two users.',
         'image': ticTacToe,
         'imageAlt': 'Screenshot of Tic-Tac-Toe game.',
         'title': 'Tic-Tac-Toe',
